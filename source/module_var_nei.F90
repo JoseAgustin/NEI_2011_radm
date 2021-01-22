fd842fab1800bbdfb85e2ecefacbc7b73be30079
Agustin Garcia <agustin@atmosfera.unam.mx>
2021-01-22 09:03:03 -0600

    Documentation

--- source/module_var_nei.F90
+++ source/module_var_nei.F90
@@ -49,14 +49,42 @@ module var_nei
   gmt,mminlu,map_proj_char
 
 contains
-!
-!  CCCC  H   H  EEEEE   CCCC  K   K
-! CC     H   H  E      CC     K K
-! C      HHHHH  EEE   C       KK
-! CC     H   H  E      CC     K K
-!  CCCC  H   H  EEEEE   CCCC  K   K
-
-subroutine check(status)
+  !       _               _
+  !   ___| |__   ___  ___| | __
+  !  / __| '_ \ / _ \/ __| |/ /
+  ! | (__| | | |  __/ (__|   <
+  !  \___|_| |_|\___|\___|_|\_\
+  !> @brief Verifies no error in netcdf function call
+  !> @param status NetCDF functions return a non-zero status codes on error.
+  !> @copyright 1993-2020 University Corporation for Atmospheric Research/Unidata
+  ! Copyright 1993-2020 University Corporation for Atmospheric Research/Unidata
+  !
+  !Portions of this software were developed by the Unidata Program at the
+  !University Corporation for Atmospheric Research.
+  !
+  !Redistribution and use in source and binary forms, with or without
+  !modification, are permitted provided that the following conditions are met:
+  !
+  ! 1. Redistributions of source code must retain the above copyright notice,
+  ! this list of conditions and the following disclaimer.
+  ! 2. Redistributions in binary form must reproduce the above copyright notice,
+  ! this list of conditions and the following disclaimer in the documentation
+  ! and/or other materials provided with the distribution.
+  ! 3. Neither the name of the copyright holder nor the names of its contributors
+  ! may be used to endorse or promote products derived from this software without
+  ! specific prior written permission.
+  ! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  ! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  ! THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  ! PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  ! CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  ! EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  ! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+  ! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  ! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  ! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+  ! OF THE POSSIBILITY OF SUCH DAMAGE.
+  subroutine check(status)
   USE netcdf
     integer, intent ( in) :: status
     if(status /= nf90_noerr) then
@@ -64,11 +92,28 @@ subroutine check(status)
       stop 2
     end if
 end subroutine check
-
+!>  @brief Reads dimensions from namelist file
+!>
+!> Obtains from domain.nml file dimension of domain dimensions.
+!>  @author Jose Agustin Garcia Reynoso
+!>  @date 01/22/2021
+!>  @version  1.0
+!>  @copyright Universidad Nacional Autonoma de Mexico
+!> @param IX number of cell grid in W-E direction
+!> @param JX number of cell grid in S-N direction
+!> @param KX number of cell grid in vertical direction
+!  _                               _
+! | | ___  ___     _ __  _ __ ___ | |
+! | |/ _ \/ _ \   | '_ \| '_ ` _ \| |
+! | |  __/  __/   | | | | | | | | | |
+! |_|\___|\___|___|_| |_|_| |_| |_|_|
+!           |_____|
 subroutine lee_nml(IX,JX,KX)
-  integer,intent(OUT)::IX,JX,KX
+  integer,intent(OUT)::IX
+  integer,intent(OUT)::JX
+  integer,intent(OUT)::KX
   integer :: nmlFileID=0
-  ! namelist-group (binds variables together, for namelist I/O).
+  ! namelist-group dom_dims.
   namelist/dom_dims/IX,JX,KX
   open(newunit=nmlFileID, file="domain.nml")
   read(nmlFileID, nml=dom_dims)
--- source/testsuite/test_nml.F90
+++ source/testsuite/test_nml.F90
@@ -1,4 +1,9 @@
 ! test_nml.F90
+!>  @brief Program to obtain the domain's dimensions
+!>  @author Jose Agustin Garcia Reynoso
+!>  @date 01/22/2021
+!>  @version  1.0
+!>  @copyright Universidad Nacional Autonoma de Mexico
 program nml_read
   use var_nei
   integer ::IX,JX,KX
