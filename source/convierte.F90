84f900848a3d23e86e6370c0ace4554f23b6b755
Agustin Garcia <agustin@atmosfera.unam.mx>
2021-01-22 20:17:26 -0600

    Documentation update

--- source/convierte.F90
+++ source/convierte.F90
@@ -1,14 +1,19 @@
 !
 !   convierte.f90
-!   
+!>  @brief Reads binary files from NEI 2011 and converts into netcdf
+!>  @details using wrfinput to set dates, attributes and dimensions for RADM2 mechanism
+!>  this is made in two steps:
+!>   1) Set hh=0 to indicate thet binary file starting at 00Z will be read
+!>      reads wrfiput file obtaining time, attibutes and dimensions.
+!>      stores 00z emissions data in netcdf format
+!>   2) Set hh=12 to read 12Z binary file
+!>      uses previous wrfinput information and stores 12z emissions
+!>      data in netcdf format
+!>  @author Jose Agustin Garcia Reynoso
+!>  @date 26/04/2018
+!>  @version  1.0
+!>  @copyright Universidad Nacional Autonoma de Mexico
 !
-!   Created by Agustin Garcia on 25/04/18.
-!   Copyright 2018 Universidad Nacional Autonoma de Mexico. All rights reserved.
-!
-!   Lee archivos binarios del NEI2011 y genera archivo Netcdf de emisiones
-!     25 abril para RADM
-!
-
 program nei_2011
 use var_nei
 
