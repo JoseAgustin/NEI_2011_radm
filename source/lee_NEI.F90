3b0d3f70b509e7241103cc8d8e913b5a5dd2d25b
Agustin Garcia <agustin@atmosfera.unam.mx>
2021-01-22 20:19:47 -0600

    Reads namelist file domain.nml

--- source/lee_NEI.F90
+++ source/lee_NEI.F90
@@ -1,10 +1,12 @@
 !
 !   lee_NEI.f90
-!   
-!
-!   Created by Agustin Garcia on 25/04/18.
-!   Copyright 2018 Universidad Nacional Autonoma de Mexico. All rights reserved.
-!
+!>  @brief Reads binary emissions file with radm categories
+!>  @details uses a global variable hh to read 00z or 12z.
+!>  @author Jose Agustin Garcia Reynoso
+!>  @date 25/04/2018
+!>  @version  1.0
+!>  @copyright Universidad Nacional Autonoma de Mexico
+!>
 !   Lee archivos binarios del NEI2011
 !     25 abril para RADM
 !     28 abril 2018 MOZART
@@ -12,12 +14,14 @@
 subroutine lee_NEI
 use var_nei
 implicit none
-  integer, parameter:: IX=159
-  integer, parameter:: JX=159
-  integer, parameter:: KX=8
   integer, parameter:: IHOUR=12
+  integer :: IX=100
+  integer :: JX=100
+  integer :: KX=2
   integer :: HR,ii,i,j,k,n
   character(len=17) :: nfile
+  call lee_nml(IX,JX,KX)
+  print *,IX,JX,KX
   nfile="wrfem_12to24z_d01"
   if(hh.eq.00) then
    nfile="wrfem_00to12z_d01"
@@ -33,13 +37,13 @@ implicit none
   if (.not. allocated(ename)) allocate(ename(NRADM+1))
   if (.not. allocated(EMISS3D)) allocate(EMISS3D(IX,KX,JX,NRADM+1,IHOUR))
   read(19) ename1
-  print *, nradm
+  write(6,'(3x,"Number of emission variables:",I3)') nradm
     ename(NRADM+1)="E_NO2"
   do n=1,NRADM
     ename(n)=ename1(n)
     if(ename1(n).eq."E_CH3COCH") ename(n)="E_CH3COCH3"
   end do
-  print *,ename
+  write(6,'(7(x,A7),A8)') ename
   do ii=1,IHOUR
     read(19) HR
     !print *,HR
